004be4ac2c11fddabdb040c48455c7ac90bacb03
Robert George <rgeorge@midnightweb.net>
2020-05-28 11:46:31 -0700

    Fix Line of Sight not rendering, and handle Line of Sight being enabled/disabled

--- src/app/core/map/layers/vision-layer.ts
+++ src/app/core/map/layers/vision-layer.ts
@@ -68,7 +68,7 @@ export class VisionLayer extends Layer {
         this.msk.beginFill(0xffffff);
 
         for(let creature of this.creatures) {
-            if(creature.vision != null && creature.vision.enabled && creature.vision.polygon != null) {
+            if(creature.vision != null && creature.light.enabled && creature.vision.polygon != null) {
 
                 let polygon = this.getGeometry(creature.vision.x, creature.vision.y, creature.vision.polygon)
                 // this might be better triangle filling function
@@ -97,7 +97,7 @@ export class VisionLayer extends Layer {
 
         // tiles, always visible
         for(let tile of this.tiles) {
-            if(tile.vision != null && tile.vision.enabled &&  tile.vision.polygon != null && tile.vision.alwaysVisible) {
+            if(tile.vision != null && tile.light.enabled &&  tile.vision.polygon != null && tile.vision.alwaysVisible) {
                 let polygon = this.getGeometry(tile.vision.x, tile.vision.y, tile.vision.polygon)
                 let shader = PIXI.Shader.from(this.vert.data, this.frag.data);
 
@@ -125,7 +125,7 @@ export class VisionLayer extends Layer {
 
         // tiles, not always visible
         for(let tile of this.tiles) {
-            if(tile.vision != null && tile.vision.enabled &&  tile.vision.polygon != null && !tile.vision.alwaysVisible) {
+            if(tile.vision != null && tile.light.enabled &&  tile.vision.polygon != null && !tile.vision.alwaysVisible) {
                 let polygon = this.getGeometry(tile.vision.x, tile.vision.y, tile.vision.polygon)
                 let shader = PIXI.Shader.from(this.vert.data, this.frag.data);
 
