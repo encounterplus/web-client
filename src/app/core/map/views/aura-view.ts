cb8f1b24906cc685e2c73595926a4a9fb34041f7
Robert George <rgeorge@midnightweb.net>
2020-07-27 18:52:57 -0700

    Added support for components on auras and tiles

--- src/app/core/map/views/aura-view.ts
+++ src/app/core/map/views/aura-view.ts
@@ -80,14 +80,174 @@ export class AuraView extends View {
             sprite.anchor.set(0.5, 0.5);
             sprite.position.set(-this.w / 2, -this.h / 2)
             sprite.width = this.w;
-	    sprite.height = this.h;
+        sprite.height = this.h;
             if (frames.length > 1) {
               if (this.aura.asset.duration === undefined) {
                 this.aura.asset.duration = 1.0;
               }
-	      sprite.animationSpeed = frames.length/this.aura.asset.duration/60.00;
-	      sprite.play();
+            sprite.animationSpeed = frames.length/this.aura.asset.duration/60.00;
+          sprite.play();
+        }
+        let ticker = PIXI.Ticker.shared;
+        for (let x=0; x < this.aura.components.length; x++) {
+            let component = this.aura.components[x];
+            if (component.enabled) {
+                if (component.type.startsWith("filter.")) {
+                    if (component.type == "filter.tint") {
+                        sprite.tint = PIXI.utils.string2hex(component.color)
+                    }
+                    if (component.type == "filter.hsb") {
+                        let hfilter = new PIXI.filters.ColorMatrixFilter();
+                        let sfilter = new PIXI.filters.ColorMatrixFilter();
+                        let bfilter = new PIXI.filters.ColorMatrixFilter();
+                        hfilter.hue(component.hue,false);
+                        sfilter.saturate(component.saturation/100,true);
+                        let l = component.brightness/100;
+                        let v = 1 + l
+                        bfilter._loadMatrix(
+                            [v,0,0,0,l,
+                             0,v,0,0,l,
+                             0,0,v,0,l,
+                             0,0,0,1,0],(l<0));
+                        sprite.filters = [hfilter,sfilter,bfilter];
+                    }
+                }
+                if (component.type.startsWith("animation.")) {
+		    let isrev = false;
+                    let loopcount = 0;
+                    let duration = component.duration || 1;
+                    let cfrom = component.from || 0;
+		    let cto = component.to || 0;
+                    if (component.type == "animation.rotation") {
+                        sprite.rotation = cfrom;
+                        ticker.add(() => {
+                            if (component.repeat && loopcount >= component.repeat) {
+                                return
+			    }
+                            let step = (cto - cfrom) / ((1000/ticker.deltaMS)*duration);
+                            if (sprite.rotation + step <= cto && !isrev) {
+                                sprite.rotation += step;
+                                if (component.autoreverse && sprite.rotation + step >= cto) {
+                                    isrev = true;
+                                } else if (component.repeat && sprite.rotation + step >= cto) {
+                                    loopcount += 1;
+                                }
+                            } else if (sprite.rotation - step >= cfrom && isrev) {
+                                sprite.rotation -= step;
+                                if (component.autoreverse && sprite.rotation - step <= cfrom) {
+                                    isrev = false;
+                                    if (component.repeat) {
+                                        loopcount += 1;
+                                    }
+                                }
+                            } else if (cto<cfrom && sprite.rotation + step >= cto && !isrev) {
+                                sprite.rotation += step;
+                                if (component.autoreverse && sprite.rotation + step <= cto) {
+                                    isrev = true;
+                                } else if (component.repeat && sprite.rotation + step <= cto) {
+                                    loopcount += 1;
+                                }
+                            } else if (cto<cfrom&&sprite.rotation - step <= cfrom && isrev) {
+                                sprite.rotation -= step;
+                                if (component.autoreverse && sprite.rotation - step >= cfrom) {
+                                    isrev = false;
+                                    if (component.repeat) {
+                                        loopcount += 1;
+                                    }
+                                }
+                            } else {
+                                sprite.rotation = cfrom;
+                            }
+                        });
+                    }
+                    if (component.type == "animation.scale") {
+                        sprite.scale.set(cfrom*(this.w/sprite.texture.frame.width));
+                        ticker.add(() => {
+                            if (component.repeat && loopcount >= component.repeat) {
+                                return
+                            }
+                            let sf = this.w/sprite.texture.frame.width;
+                            let step = ((cto*sf) - (cfrom*sf)) / ((1000/ticker.deltaMS)*duration);
+                            if (sprite.scale.x + step <= cto*sf && !isrev) {
+                                sprite.scale.set( sprite.scale.x + step );
+                                if (component.autoreverse && sprite.scale.x + step >= cto*sf) {
+                                    isrev = true;
+                                } else if (component.repeat && sprite.scale.x + step >= cto*sf) {
+                                    loopcount += 1;
+                                }
+                            } else if (sprite.scale.x - step >= cfrom*sf && isrev) {
+                                sprite.scale.set( sprite.scale.x - step );
+                                if (component.autoreverse && sprite.scale.x - step <= cfrom*sf) {
+                                    isrev = false;
+                                    if (component.repeat) {
+                                        loopcount += 1;
+                                    }
+                                }
+                            } else if (cto<cfrom&&sprite.scale.x + step >= cto*sf && !isrev) {
+                                sprite.scale.set( sprite.scale.x + step );
+                                if (component.autoreverse && sprite.scale.x + step <= cto*sf) {
+                                    isrev = true;
+                                } else if (component.repeat && sprite.scale.x + step <= cto*sf) {
+                                    loopcount += 1;
+                                }
+                            } else if (cto<cfrom&&sprite.scale.x - step <= cfrom*sf && isrev) {
+                                sprite.scale.set( sprite.scale.x - step )
+                                if (component.autoreverse && sprite.scale.x - step >= cfrom*sf) {
+                                    isrev = false;
+                                    if (component.repeat) {
+                                        loopcount += 1;
+                                    }
+                                }
+                            } else {
+                                sprite.scale.set(cfrom*sf);
+                            }
+                        });
+                    }
+                    if (component.type == "animation.opacity") {
+                        sprite.alpha = cfrom;
+                        ticker.add(() => {
+                            if (component.repeat && loopcount >= component.repeat) {
+                                return
+                            }
+                            let step = (cto - cfrom) / ((1000/ticker.deltaMS)*duration);
+                            if (sprite.alpha + step <= cto && !isrev) {
+                                sprite.alpha += step;
+                                if (component.autoreverse && sprite.alpha + step >= cto) {
+                                    isrev = true;
+                                } else if (component.repeat && sprite.alpha + step >= cto) {
+                                    loopcount += 1;
+                                }
+                            } else if (sprite.alpha - step >= cfrom && isrev) {
+                                sprite.alpha -= step;
+                                if (component.autoreverse && sprite.alpha - step <= cfrom) {
+                                    isrev = false;
+                                    if (component.repeat) {
+                                        loopcount += 1;
+                                    }
+                                }
+                            } else if (cto<cfrom&&sprite.alpha + step >= cto && !isrev) {
+                                sprite.alpha += step;
+                                if (component.autoreverse && sprite.alpha + step <= cto) {
+                                    isrev = true;
+                                } else if (component.repeat && sprite.alpha + step <= cto) {
+                                    loopcount += 1;
+                                }
+                            } else if (cto<cfrom&&sprite.alpha - step <= cfrom && isrev) {
+                                sprite.alpha -= step;
+                                if (component.autoreverse && sprite.alpha - step >= cfrom) {
+                                    isrev = false;
+                                    if (component.repeat) {
+                                        loopcount += 1;
+                                    }
+                                }
+                            } else {
+                                sprite.alpha = cfrom
+                            }
+                        });
+                    }
+                }
             }
+        }
         }
 
         return this;
--- src/app/shared/models/component.ts
+++ src/app/shared/models/component.ts
@@ -1,4 +1,12 @@
 export class Component {
     type: string;
     enabled: string;
+    duration: number;
+    from: number;
+    to: number;
+    autoreverse: boolean;
+    color: string;
+    hue: number;
+    saturation: number;
+    brightness: number;
 }
