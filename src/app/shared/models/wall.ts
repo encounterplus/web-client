322a92a3e053da9d7034609929e864fdbb550175
Juraj <juraj@qbit.sk>
2020-08-19 14:47:08 +0200

    add new model for walls

--- src/app/shared/models/wall.ts
+++ src/app/shared/models/wall.ts
@@ -1,6 +1,29 @@
+export enum WallType {
+    normal = "normal",
+    invisible = "invisible",
+    ethereal = "ethereal",
+    terrain = "terrain",
+    door = "door",
+    secretDoor = "secretDoor"
+}
+
+export enum WallSide {
+    both = "both",
+    left = "left",
+    right = "right"
+}
+
+export enum DoorState {
+    closed = "closed",
+    open = "open",
+    locked = "locked"
+}
+
 export class Wall {
-    data: Array<number> = [];
-    strokeWidth: number;
-    strokeColor: string;
-    opacity: number;
+    id: string
+    data: Array<number> = []
+    color: string
+    type: WallType = WallType.normal
+    side: WallSide = WallSide.both
+    door: DoorState
 }
